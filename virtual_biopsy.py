a625a7c745b440073b158185bff81bf060ccf20a
lkhangkv1995 <98082429+lkhangkv1995@users.noreply.github.com>
2022-10-17 11:30:01 +0900

    Update virtual_biopsy.py

--- virtual_biopsy.py
+++ virtual_biopsy.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from xml.dom import minidom
 from options import options
 import options
+import math
 
 options = options.get_options()
 
@@ -26,84 +27,113 @@ def read_xml(xml_path):
             pairs.append(pair)
     return pairs
 
-def get_biopsy(start_location, direct_location, wsi, pixel_length, pixel_radius):
-    mpp = float(wsi.properties['openslide.mpp-x'])
-    points = [start_location, direct_location]
+def rotation_remap(x, y, theta, img_size):
+    theta = math.radians(-theta)
+    x_center, y_center = img_size[0]/2, img_size[1]/2
+    x, y = x-x_center, y-y_center
+    x_remap = x*math.cos(theta) - y*math.sin(theta)
+    y_remap = x*math.sin(theta) + y*math.cos(theta)
+    x_remap, y_remap = x_remap+x_center, y_remap+y_center
+    x_remap = max(x_remap, 0)
+    x_remap = min(x_remap, img_size[0])
+    y_remap = max(y_remap, 0)
+    y_remap = min(y_remap, img_size[1])
+    return x_remap, y_remap
 
+
+def get_biopsy(start_location, direct_location, wsi, pixel_length, pixel_radius):
+    mpp = (float(wsi.properties['openslide.mpp-x']) + float(wsi.properties['openslide.mpp-x']))/2
     x1, y1 = start_location
     x2, y2 = direct_location
-    distance_x = (x2-x1)
-    distance_y = (y2-y1)
-    
-    if distance_x > 0:
-        ratio = distance_y/distance_x
-        current_location = start_location
-        # create biopy
-        diagonal = 0
-        current_img = (torch.zeros(3,pixel_radius,1) if abs(ratio)<=1 else torch.zeros(3,1,pixel_radius))
-        size = ((1, pixel_radius) if abs(ratio)<=1 else (pixel_radius,1))
-        dim = (2 if abs(ratio)<=1 else 1)
-        move = (1 if y1 < y2 else -1)
-        to_tensor = transforms.ToTensor()
-        to_img = transforms.ToPILImage()
-        while diagonal <= pixel_length:
-            current_location = ((current_location[0] + 1, current_location[1] + ratio) if abs(ratio)<=1 else (current_location[0] + move/ratio, current_location[1] + move))
-            each_img = wsi.read_region(location=(int(current_location[0]), int(current_location[1])),size=size, level=0)
-            each_img = to_tensor(each_img)[:3]
-            current_img = torch.cat([current_img, each_img], dim=dim)
-            diagonal = ((current_location[0]-start_location[0])**2 + (current_location[1]-start_location[1])**2)**(1/2)
-            
-        current_img = (to_img(current_img) if abs(ratio)<=1 else to_img(current_img.transpose(1,2)))
-    elif distance_x < 0:
-        ratio = distance_y/distance_x
-        current_location = start_location
-        # create biopy
-        diagonal = 0
-        current_img = (torch.zeros(3,pixel_radius,1) if abs(ratio)<=1 else torch.zeros(3,1,pixel_radius))
-        size = ((1, pixel_radius) if abs(ratio)<=1 else (pixel_radius,1))
-        dim = (2 if abs(ratio)<=1 else 1)
-        move = (1 if y1 < y2 else -1)
-        to_tensor = transforms.ToTensor()
-        to_img = transforms.ToPILImage()
-        while diagonal <= pixel_length:
-            current_location = ((current_location[0] - 1, current_location[1] - ratio) if abs(ratio)<=1 else (current_location[0] - move/ratio, current_location[1] - move))
-            each_img = wsi.read_region(location=(int(current_location[0]), int(current_location[1])),size=size, level=0)
-            each_img = to_tensor(each_img)[:3]
-            current_img = torch.cat([current_img, each_img], dim=dim)
-            diagonal = ((current_location[0]-start_location[0])**2 + (current_location[1]-start_location[1])**2)**(1/2)
-            
-        current_img = (to_img(current_img) if abs(ratio)<=1 else to_img(current_img.transpose(1,2)))
+
+    x_direction = ((x2-x1)/abs(x2-x1) if x2-x1 != 0 else 0)
+    y_direction = ((y2-y1)/abs(y2-y1) if y2-y1 != 0 else 0)
+
+
+    x3 = x1 + pixel_length * x_direction
+    y3 = y1 + pixel_length * y_direction
+
+
+    if x_direction != 0 and y_direction != 0:
+
+        tan = (x2-x1)/(y2-y1)
+        angle = (math.atan(tan) if x_direction*y_direction > 0 else -math.atan(tan))
+        angle_deg = math.degrees( angle )
+
+        x3 = x1 + pixel_length * math.sin(angle) * x_direction
+        y3 = y1 + pixel_length * math.cos(angle) * y_direction
+
+        diagonal_length = (pixel_radius**2 + pixel_length**2)**(1/2)
+        center_x = x1 + 0.5 * pixel_length * math.sin(angle) * x_direction
+        center_y = y1 + 0.5 * pixel_length * math.cos(angle) * y_direction
+        crop_x = center_x - 0.5 * pixel_length
+        crop_y = center_y - 0.5 * pixel_length
+        crop_location = ((int(crop_x), int(crop_y)))
+        box_size = (int(diagonal_length)+2, int(diagonal_length)+2)
+        img = wsi.read_region(location=crop_location, size=box_size, level=0)
+        x1_before_rotate = x1 - crop_location[0]
+        y1_before_rotate = y1 - crop_location[1]
+        x3_before_rotate = x3 - crop_location[0]
+        y3_before_rotate = y3 - crop_location[1]
+        if x_direction > 0 and y_direction > 0:
+            rotation_angle = 90-angle_deg
+        elif x_direction > 0 and y_direction < 0:
+            rotation_angle = angle_deg-90
+        elif x_direction < 0 and y_direction > 0:
+            rotation_angle = 90+angle_deg
+        elif x_direction < 0 and y_direction < 0:
+            rotation_angle =-90-angle_deg
+
+
+        x1_after_rotate, y1_after_rotate = rotation_remap(x=x1_before_rotate,
+                                                         y=y1_before_rotate,
+                                                         theta=rotation_angle,
+                                                         img_size=box_size)
+        x3_after_rotate, y3_after_rotate = rotation_remap(x=x3_before_rotate,
+                                                         y=y3_before_rotate,
+                                                         theta=rotation_angle,
+                                                         img_size=box_size)
+
+
+        rotated_img = img.rotate(rotation_angle)
+        top = max(min(y1_after_rotate - 0.5*pixel_radius, y1_after_rotate + 0.5*pixel_radius, 
+                   y3_after_rotate - 0.5*pixel_radius, y3_after_rotate + 0.5*pixel_radius),0)
+        bottom = min(max(y1_after_rotate - 0.5*pixel_radius, y1_after_rotate + 0.5*pixel_radius, 
+                   y3_after_rotate - 0.5*pixel_radius, y3_after_rotate + 0.5*pixel_radius), box_size[1])
+        left = max(min(x1_after_rotate, x3_after_rotate), 0)
+        right = min(max(x1_after_rotate, x3_after_rotate), box_size[0])
+        sample = rotated_img.crop((left, top, right, bottom))
     else:
-        current_location = start_location
-        diagonal = 0
-        current_img = torch.zeros(3,1,pixel_radius)
-        to_tensor = transforms.ToTensor()
-        to_img = transforms.ToPILImage()
-        move = (1 if y1 < y2 else -1)
-        while diagonal <= pixel_length:
-            current_location = (current_location[0], current_location[1] + move)
-            each_img = wsi.read_region(location=(current_location[0], int(current_location[1])),size=(pixel_radius,1), level=0)
-            each_img = to_tensor(each_img)[:3]
-            current_img = torch.cat([current_img, each_img], dim=1)
-            diagonal = ((current_location[0]-start_location[0])**2 + (current_location[1]-start_location[1])**2)**(1/2)
-        current_img = to_img(current_img.transpose(1,2))
+        if x_direction == 0 and y_direction > 0:
+            sample = wsi.read_region(location=(int(x1-pixel_radius*0.5),int(y1)), size=(int(pixel_length), int(pixel_length)), level=0)
+            sample = sample.rotate(90)
+            sample = sample.crop((0, (pixel_length-pixel_radius)/2, pixel_length, (pixel_length+pixel_radius)/2))
+        elif x_direction == 0 and y_direction < 0:
+            sample = wsi.read_region(location=(int(x3-pixel_radius*0.5),int(y3)), size=(int(pixel_length), int(pixel_length)), level=0)
+            sample = sample.crop((0, (pixel_length-pixel_radius)/2, pixel_length, (pixel_length+pixel_radius)/2))
+        elif x_direction > 0 and y_direction == 0:
+            sample = wsi.read_region(location=(int(x1),int(y1-pixel_radius*0.5)), size=(int(pixel_length), int(pixel_length)), level=0)
+            sample = sample.crop((0, (pixel_length-pixel_radius)/2, pixel_length, (pixel_length+pixel_radius)/2))
+        elif x_direction < 0 and y_direction == 0:
+            sample = wsi.read_region(location=(int(x3),int(y3-pixel_radius*0.5)), size=(int(pixel_length), int(pixel_length)), level=0)
+            sample = sample.rotate(180)
+            sample = sample.crop((0, (pixel_length-pixel_radius)/2, pixel_length, (pixel_length+pixel_radius)/2))
+  
             
-    return current_img
+    return sample
 
 def make_biopsy(wsi_path, start_location, direct_location):
     wsi = openslide.OpenSlide(wsi_path)
-    mpp_x = float(wsi.properties['openslide.mpp-x'])
-    mpp_y = float(wsi.properties['openslide.mpp-y'])
+    mpp = (float(wsi.properties['openslide.mpp-x']) + float(wsi.properties['openslide.mpp-x']))/2
     biopsy_length = np.random.normal(options['length'], options['length_sd']) * 1000 # convert to micron
     biopsy_radius = np.random.normal(options['radius'], options['radius_sd']) * 1000
-    pixel_length = int(biopsy_length/mpp_x)
-    pixel_radius = int(biopsy_radius/mpp_x)
+    pixel_length = int(biopsy_length/mpp)
+    pixel_radius = int(biopsy_radius/mpp)
     sample = get_biopsy(start_location=start_location, 
                  direct_location=direct_location, 
                  wsi=wsi, 
                  pixel_length=pixel_length, 
                  pixel_radius=pixel_radius)
-    sample = sample.resize((int(pixel_length),int(pixel_radius)))
     return sample
 
 def make_slide(samples):
